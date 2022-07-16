d1069858b16dbfc4034961b5546893fa3d66f64a
minhtue510 <87066368+minhtue510@users.noreply.github.com>
2022-07-16 14:11:55 +0700

    React Redux Forms Revisited

--- .history/src/redux/configureStore_20220716140455.js
+++ .history/src/redux/configureStore_20220716140455.js
@@ -3,10 +3,9 @@ import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
-import { createForms } from 'react-redux-form';
-import { InitialFeedback } from './forms';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -14,12 +13,8 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders,
-            ...createForms({
-                feedback: InitialFeedback
-            })
+            leaders: Leaders
         }),
-
         applyMiddleware(thunk, logger)
     );
     
--- .history/src/redux/configureStore_20220716140459.js
+++ .history/src/redux/configureStore_20220716140459.js
@@ -3,10 +3,10 @@ import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
-import { createForms } from 'react-redux-form';
-import { InitialFeedback } from './forms';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -14,12 +14,8 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders,
-            ...createForms({
-                feedback: InitialFeedback
-            })
+            leaders: Leaders
         }),
-
         applyMiddleware(thunk, logger)
     );
     
